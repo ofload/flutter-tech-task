021382e9a35ddde3456746b904c1ecf36310fc23
Wisnu WH <wisnu@altrovis.com>
2019-12-10 20:49:56 +0700

    add luch page and set it as starting page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tech_task/screens/lunch_page.dart';
 
 String root = "https://lb7u7svcm5.execute-api.ap-southeast-1.amazonaws.com/dev";
 
@@ -8,58 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loadsmile Lunch App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: LunchPage(),
     );
   }
 }
---
+++ lib/screens/lunch_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:intl/intl.dart';
+
+class LunchPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LunchState();
+  }
+}
+
+class _LunchState extends State<LunchPage> {
+
+  TextEditingController _lunchDatePickerController = new MaskedTextController(mask: '0000-00-00');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Loadsmile Lunch App'),
+      ),
+      body: _buildPage(context),
+    );
+  }
+
+  Widget _buildPage(BuildContext context) {
+
+    final children = <Widget>[];
+
+    final lunchDate = buildLunchDate(context);
+    children.add(buildLabel('Lunch Date'));
+    children.add(lunchDate);
+
+    final submitButton = buildSubmitButton(context);
+    children.add(submitButton);
+    
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: children,
+    );
+  }
+
+  Widget buildLabel(String label) {
+    final labelContainer =  Container(
+      padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+      child: Text(
+        label,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+          ),
+        ),
+     );
+    return labelContainer;
+  }
+
+  Widget buildLunchDate(BuildContext context) {
+
+    final lunchDate = Container(
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: TextField(
+        controller: _lunchDatePickerController,
+        keyboardType: TextInputType.datetime,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            suffixIcon: InkResponse(
+            onTap: () => _selectDate(context, _lunchDatePickerController),
+            child: Icon(Icons.date_range),
+          ),
+        ),
+      ),
+    );
+
+    return lunchDate;
+  }
+
+  Future<Null> _selectDate(BuildContext context, TextEditingController _datePickerController) async {
+    DateTime selectedDate = DateTime.now();
+    final DateTime pickedDate = await showDatePicker(
+        context: context,
+        firstDate: DateTime(2019),
+        initialDate: selectedDate,
+        lastDate: DateTime(2030));
+    if (pickedDate != null && pickedDate != selectedDate)
+      setState(() {
+        selectedDate = pickedDate;
+        _datePickerController.text = DateFormat('yyyy-MM-dd').format(selectedDate);
+      },
+    );
+  }
+
+  Widget buildSubmitButton(BuildContext context) {
+    final submitButton = Container(
+      margin: EdgeInsets.all(20),
+      height: MediaQuery.of(context).size.height * 0.09,
+      child: RaisedButton(
+        onPressed: (){},
+        color: Colors.orange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Text('Get Ingredients',
+          textAlign: TextAlign.center, style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+    
+    return submitButton;
+  }
+
+}
