1a143ac255e09857bfa01c818ab2247a0497b01c
Wisnu WH <wisnu@altrovis.com>
2019-12-11 11:20:55 +0700

    add ingredient page with mock data

--- lib/models/ingredients.dart
+++ lib/models/ingredients.dart
@@ -2,6 +2,7 @@ class Ingredients {
   final String title;
   final String useBy;
 
+  Ingredients({this.title, this.useBy});
   Ingredients.fromJson(Map<String, dynamic> parsedJson)
     : title = parsedJson["title"],
       useBy = parsedJson["use-by"];
--- lib/screens/ingredient_page.dart
+++ lib/screens/ingredient_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tech_task/models/ingredients.dart';
 
 class IngredientPage extends StatefulWidget {
   @override
@@ -9,7 +10,6 @@ class IngredientPage extends StatefulWidget {
 
 class _IngredientState extends State<IngredientPage> {
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,16 +17,54 @@ class _IngredientState extends State<IngredientPage> {
         title: Text('Lunch Ingredients'),
       ),
       body: _buildPage(context),
+       floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.pushNamed(context, '/recipe');
+        },
+        child: Icon(Icons.next_week),
+      ),
     );
   }
 
   Widget _buildPage(BuildContext context) {
-
     final children = <Widget>[];
-    
-    return ListView(
-      padding: EdgeInsets.zero,
+   
+    final listNotification = buildList();
+    final container = Container(
+      height: MediaQuery.of(context).size.height - 130,
+      child: listNotification,
+    );
+    children.add(container);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: children,
+    );
+
+  }
+
+  Widget buildList() {
+    final List<Ingredients> ingredients = <Ingredients>[
+      Ingredients(title: "Ham", useBy: "2019-11-25"),
+      Ingredients(title: "Cheese", useBy: "2019-11-25"),
+      Ingredients(title: "Bread", useBy: "2019-11-25"),
+      Ingredients(title: "Butter", useBy: "2019-11-25"),
+      Ingredients(title: "Bacon", useBy: "2019-11-25"),
+      Ingredients(title: "Eggs", useBy: "2019-11-25"),
+    ];
+
+    return ListView.builder(
+      itemCount: ingredients.length,
+      itemBuilder: (context, index) {
+        return Center(
+          child: CheckboxListTile(
+            title: Text('${ingredients[index].title}'),
+            subtitle: Text('use-by : ${ingredients[index].useBy}'),
+            value: false,
+            onChanged: (bool value) { },
+          ),
+        );
+      }
     );
   }
 }
